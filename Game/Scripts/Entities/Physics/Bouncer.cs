b1d0916878e9cbe411048c2662f95ee9e6e38de5
Ruan PA <ruan@inkdev.net>
2012-02-22 21:37:13 +0000

    Bouncy now bounces itself, rather than the colliding entity

--- Game/Scripts/Entities/Physics/Bouncer.cs
+++ Game/Scripts/Entities/Physics/Bouncer.cs
@@ -7,21 +7,24 @@ namespace CryGameCode.Entities
 	/// </summary>
 	public class Bouncy : Entity
 	{
+		[EditorProperty]
+		public float BounceMultiplier { get; set; }
+
+		[EditorProperty(DefaultValue = 10)]
+		public float Mass { get { return Physics.Mass; } set { Physics.Mass = value; } }
+
 		protected override void OnCollision(uint targetEntityId, Vec3 hitPos, Vec3 dir, short materialId, Vec3 contactNormal)
 		{
-			StaticEntity targetEnt = EntitySystem.GetEntity(targetEntityId);
-			if (targetEnt != null)
-			{
-				targetEnt.Physics.AddImpulse(new Vec3(0,0,9.81f * 20));
-			}
+			Console.LogAlways("Bouncy.OnCollision (ID = {0}), targetID = {1}, hitpos = {2}, dir = {3}, materialId = {4}, normal = {5} | strength is {6}",
+				this.Id, targetEntityId, hitPos, dir, materialId, contactNormal, -GlobalPhysics.GravityZ * BounceMultiplier);
+
+			Physics.AddImpulse(new Vec3(0, 0, -GlobalPhysics.GravityZ * BounceMultiplier));
 		}
 
 		protected override void OnReset(bool enteringGame)
 		{
 			LoadObject(Model);
 			Physics.Type = PhysicalizationType.Rigid;
-			Physics.Stiffness = 70;
-			Physics.Mass = 10;
 		}
 
 		[EditorProperty(Type = EntityPropertyType.Object)]
