2f09d59e3d7a7c75813773db7b4a64508f442804
Ruan PA <ruan@inkdev.net>
2012-02-23 04:35:04 +0000

    100% more 4:30am game code, 100% less fatal errors

--- Game/Scripts/Entities/Actor/PlayableCharacter.cs
+++ Game/Scripts/Entities/Actor/PlayableCharacter.cs
@@ -3,37 +3,44 @@ using CryGameCode.Entities;
 
 namespace CryGameCode
 {
-    public class Player : BasePlayer
-    {
-        public Player() 
-        {
+	public class Player : BasePlayer
+	{
+		public Player()
+		{
 			InputSystem.RegisterAction("move_right", OnMoveRight);
 			InputSystem.RegisterAction("move_left", OnMoveLeft);
-
+			InputSystem.RegisterAction("move_forward", OnJump);
 			ReceiveUpdates = true;
-
-			//Spawn the player camera
-			var playerCamera = EntitySystem.SpawnEntity<PlayerCamera>("Camera");
-
-			playerCamera.Camera = new Camera { FieldOfView = 60 };
-			playerCamera.Target = this;
-        }
+		}
 
 		public void OnRevive()
 		{
 			LoadObject(@"Objects/default/primitive_sphere.cgf");
 			Physics.Type = PhysicalizationType.Rigid;
 			Physics.Mass = 100;
+
+			//Spawn the player camera
+			var playerCamera = EntitySystem.SpawnEntity<PlayerCamera>("Camera", new Vec3(), new Vec3(), new Vec3(1, 1, 1));
+
+			playerCamera.Camera = new Camera { FieldOfView = 60 };
+			playerCamera.Target = this;
+
+			Renderer.Camera = playerCamera.Camera;
 		}
 
 		public void OnMoveRight(ActionActivationMode activationMode, float value)
 		{
-			Physics.AddImpulse(new Vec3 { Y = 10 });
+			Physics.AddImpulse(new Vec3 { Y = 100 });
 		}
 
 		public void OnMoveLeft(ActionActivationMode activationMode, float value)
 		{
-			Physics.AddImpulse(new Vec3 { Y = -10 });
+			Physics.AddImpulse(new Vec3 { Y = -100 });
 		}
-    }
+
+		public void OnJump(ActionActivationMode activationMode, float value)
+		{
+			Physics.AddImpulse(new Vec3 { Z = 100 });
+		}
+	}
 }
