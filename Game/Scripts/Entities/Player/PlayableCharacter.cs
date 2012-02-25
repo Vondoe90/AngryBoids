c7b2ba43513be935760cb8d8d52300d5eadcb53f
Filip Lundgren <filip@inkdev.net>
2012-02-25 02:00:37 +0100

    The current view instance is now handled entirely in C# instead of C++.

--- Game/Scripts/Entities/Player/PlayableCharacter.cs
+++ Game/Scripts/Entities/Player/PlayableCharacter.cs
@@ -9,7 +9,7 @@ namespace CryGameCode
 		{
 			InputSystem.RegisterAction("move_right", OnMoveRight);
 			InputSystem.RegisterAction("move_left", OnMoveLeft);
-			InputSystem.RegisterAction("move_forward", OnJump);
+			InputSystem.RegisterAction("jump", OnJump);
 			ReceiveUpdates = true;
 		}
 
@@ -20,12 +20,9 @@ namespace CryGameCode
 			Physics.Mass = 100;
 
 			//Spawn the player camera
-			var playerCamera = EntitySystem.SpawnEntity<PlayerCamera>("Camera", new Vec3(), new Vec3(), new Vec3(1, 1, 1));
+			PlayerCam = EntitySystem.SpawnEntity<PlayerCamera>("Camera", new Vec3(), new Vec3(), new Vec3(1, 1, 1));
 
-			playerCamera.Camera = new Camera { FieldOfView = 60 };
-			playerCamera.Target = this;
-
-			Renderer.Camera = playerCamera.Camera;
+			PlayerCam.Target = this;
 		}
 
 		public void OnMoveRight(ActionActivationMode activationMode, float value)
@@ -40,7 +37,9 @@ namespace CryGameCode
 
 		public void OnJump(ActionActivationMode activationMode, float value)
 		{
-			Physics.AddImpulse(new Vec3 { Z = 10 });
+			Physics.AddImpulse(new Vec3 { Z = 1 });
 		}
+
+		PlayerCamera PlayerCam;
 	}
 }
--- Game/Scripts/Entities/Player/PlayerCamera.cs
+++ Game/Scripts/Entities/Player/PlayerCamera.cs
@@ -4,16 +4,25 @@ namespace CryGameCode.Entities
 {
 	public class PlayerCamera : Entity
 	{
-		public Camera Camera { get; set; }
-		public Player Target { get; set; }
-
 		public override void OnUpdate()
 		{
-			if(Camera == null || Target == null)
+			if(Target == null)
 				return;
 
-			Camera.Position = Target.Position - new Vec3(5, 5, 5);
+			Camera.Position = Target.Position + new Vec3(0, 0, 10);
 			Camera.Angles = new Vec3(0, 0, 0);
+
+			Renderer.Camera = Camera;
 		}
+
+		public override void OnSpawn()
+		{
+			Camera = new Camera { FieldOfView = 60 };
+
+			Renderer.Camera = Camera;
+		}
+
+		public Camera Camera;
+		public Player Target { get; set; }
 	}
 }
