b4e2fdc2b73669ed78aac98e5701f752f2a5c470
Filip Lundgren <filip@inkdev.net>
2012-03-17 23:18:11 +0100

    Added EventSystem test. (highly WIP)

---
+++ Game/Scripts/UI/MyEventSystem.cs
@@ -0,0 +1,24 @@
+﻿using CryEngine;
+
+namespace CryGameCode.BoidUIEvents
+{
+	[UIEvent(Name = "OnBoidLaunched", Description = "Triggered when a boid is shot", Category = "BoidEvents")]
+	public class OnBoidLaunched
+	{
+		[Port(Name = "BoidName", Description = "Name of the launched boid")]
+		public OutputPort<string> boidType;
+
+		[Port(Name = "Remaining Boids", Description = "Amount of boids left to launch")]
+		public OutputPort<int> boidsLeft;
+	}
+
+	[UIEvent(Name = "SetDifficulty", Description = "Sets Angry Boids difficulty level", Category = "BoidEvents")]
+	public class SetDifficulty
+	{
+		[Port(Name = "DifficultyLevel", Description = "The difficulty level we want to set!")]
+		public void SetDifficultyLevel(string level)
+		{
+			// Do whatever logic is required to change the diff level.
+		}
+	}
+}
