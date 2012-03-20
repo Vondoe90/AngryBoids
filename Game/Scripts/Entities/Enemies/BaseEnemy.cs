6fb516dba6c1f54492a0030097f59f9fe113c943
Filip Lundgren <filip@inkdev.net>
2012-03-20 17:48:15 +0100

    Added Enemy OnKill particle effect.

--- Game/Scripts/Entities/Enemies/BaseEnemy.cs
+++ Game/Scripts/Entities/Enemies/BaseEnemy.cs
@@ -11,7 +11,12 @@ namespace CryGameCode.AngryBoids
 		protected override void OnCollision(EntityId targetEntityId, Vec3 hitPos, Vec3 dir, short materialId, Vec3 contactNormal)
 		{
 			if(Velocity.Length > VelocityToKill)
+			{
+				var particle = ParticleEffect.Get("explosions.barrel.explode");
+				particle.Spawn(hitPos);
+
 				Remove();
+			}
 		}
 	}
 }
