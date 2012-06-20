873bfef01fc1a1db3cee029d2c7f6ad46c6857da
Sam Neirinck <github@samneirinck.be>
2012-06-20 22:42:38 +0200

    Async demo

--- Game/Scripts/Entities/Boids/TheBoringOne.cs
+++ Game/Scripts/Entities/Boids/TheBoringOne.cs
@@ -1,4 +1,7 @@
-﻿using CryEngine;
+﻿using System;
+using System.Threading.Tasks;
+using CryEngine;
+using CryEngine.Async;
 
 namespace CryGameCode.Entities.AngryBoids
 {
@@ -9,5 +12,96 @@ namespace CryGameCode.Entities.AngryBoids
 			: base()
 		{
 		}
+
+        protected override void OnReset(bool enteringGame)
+        {
+            if (enteringGame)
+                Test();
+            base.OnReset(enteringGame);
+        }
+
+        public async void Test()
+        {
+            Debug.Log("Hello there");
+            await Delay.TimeDelay(TimeSpan.FromSeconds(30));
+            Debug.LogAlways("Are you there?");
+            await Delay.TimeDelay(TimeSpan.FromSeconds(20));
+
+            await LoopMode();
+        }
+
+	    public async Task LoopMode()
+	    {
+            var originalPosition = this.Position;
+            //Debug.Log("Starting to move");
+            //await MoveTo(new Vec3(900, 900, 100), TimeSpan.FromSeconds(1));
+            //Debug.LogAlways("Arrived at destination, moving back");
+            //await MoveTo(originalPosition, TimeSpan.FromSeconds(1));
+            //await LoopMode();
+	        
+	    }
+
+        public Task MoveTo(Vec3 position, TimeSpan duration)
+        {
+            var job = new MoveToJob(this, position, duration);
+            job.Task.ConfigureAwait(false).GetAwaiter();
+            Awaiter.Instance.Jobs.Add(job);
+            return job.Task;
+        }
 	}
+
+    public class MoveToJob : AsyncJob<bool>
+    {
+
+        // TODO: Refactor to use decent timing instead of DateTime
+
+        public float DelayInMilliseconds { get; set; }
+
+        private DateTime beginTime;
+        private DateTime endTime;
+        private EntityBase entity;
+        private Vec3 beginPosition;
+        private Vec3 endPosition;
+
+        public MoveToJob(float milliseconds)
+            : base()
+        {
+            DelayInMilliseconds = milliseconds;
+            beginTime = DateTime.Now;
+
+            if (milliseconds <= 0)
+            {
+                source.TrySetResult(false);
+                IsFinished = true;
+            }
+        }
+
+        public MoveToJob(EntityBase ent, Vec3 position, TimeSpan delay)
+        {
+            DelayInMilliseconds = (float)delay.TotalMilliseconds;
+            beginTime = DateTime.Now;
+            endTime = DateTime.Now + delay;
+            beginPosition = ent.Position;
+            endPosition = position;
+            entity = ent;
+        }
+
+        public override bool Update(float frameTime)
+        {
+            var now = DateTime.Now;
+            if (!IsFinished)
+            {
+                float percentageElapsed = System.Math.Min(((float)(now - beginTime).TotalMilliseconds )/DelayInMilliseconds, 1);
+
+                var diff = endPosition - beginPosition;
+                entity.Position = new Vec3(beginPosition.X + diff.X * percentageElapsed, beginPosition.Y + diff.Y * percentageElapsed, beginPosition.Z + diff.Z * percentageElapsed);
+                if (now >= endTime)
+                {
+                    source.TrySetResult(true);
+                    IsFinished = true;
+                }
+            }
+            return IsFinished;
+        }
+    }
 }
--- Game/Scripts/GameRules/SinglePlayer.cs
+++ Game/Scripts/GameRules/SinglePlayer.cs
@@ -1,9 +1,13 @@
+using System.Threading.Tasks;
 using CryEngine;
+using CryEngine.Async;
 using CryGameCode.AngryBoids;
 using CryGameCode.Entities;
 
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using CryGameCode.Entities.AngryBoids;
 
 /// <summary>
 /// The campaign game mode is the base game mode
@@ -40,7 +44,57 @@ namespace CryGameCode
 				return;
 			}
 
+            Debug.Log("Current = " + Environment.Version.ToString());
+            foreach (var item in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                Debug.Log("Loaded " + item.FullName);
+            }
+
+		    StartMovingTargets();
+
 			cameraProxy.Init();
 		}
+
+        private async void StartMovingTargets()
+        {
+            var enemy1 = Entity.Find("Enemy1");
+            var enemy2 = Entity.Find("Enemy2");
+            var enemy3 = Entity.Find("Enemy3");
+
+            var pos1 = enemy1.Position;
+            var pos2 = enemy2.Position;
+            var pos3 = enemy3.Position;
+
+            await DoAnimationLoop(enemy1, enemy2, enemy3, pos1, pos2, pos3);
+        }
+
+        private async Task DoAnimationLoop(EntityBase enemy1, EntityBase enemy2, EntityBase enemy3, Vec3 pos1, Vec3 pos2, Vec3 pos3)
+        {
+
+            var speed = TimeSpan.FromMilliseconds(800);
+            
+            MoveTo(enemy1, pos2, speed);
+            MoveTo(enemy2, pos3, speed);
+            await MoveTo(enemy3, pos1, speed);
+
+            MoveTo(enemy1, pos3, speed);
+            MoveTo(enemy2, pos1, speed);
+            await MoveTo(enemy3, pos2, speed);
+
+            MoveTo(enemy1, pos1, speed);
+            MoveTo(enemy2, pos2, speed);
+            await MoveTo(enemy3, pos3, speed);
+
+            await DoAnimationLoop(enemy1, enemy2, enemy3, pos1, pos2, pos3);
+        }
+
+        public Task MoveTo(EntityBase ent, Vec3 position, TimeSpan duration)
+        {
+            var job = new MoveToJob(ent, position, duration);
+            job.Task.ConfigureAwait(false).GetAwaiter();
+            Awaiter.Instance.Jobs.Add(job);
+            return job.Task;
+        }
+
 	}
 }
